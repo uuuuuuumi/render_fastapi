ec5b71f76ca3e635c8ef7b41f676b3fcff645479
uuuuuuumi <166776882+uuuuuuumi@users.noreply.github.com>
2024-06-14 13:56:33 +0900

    HowOldAreYou

--- main.py
+++ main.py
@@ -42,9 +42,59 @@ def index():
         <head>
             <title>Some HTML in here</title>
         </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
+       <body>
+    <h1>自己紹介</h1>
+
+    <section>
+        <h2>基本情報</h2>
+        <ul>
+            <li>名前：非公開</li>
+            <li>誕生日：2003年10月10日</li>
+            <li>趣味：
+                <ul>
+                    <li>旅行に行くこと</li>
+                    <li>星空を見ること</li>
+                    <li>写真を撮ること</li>
+                    <li>ドライブ</li>
+                </ul>
+            </li>
+            <li>おすすめの旅行先：岐阜</li>
+            <li>バイト：
+                <ul>
+                    <li>焼き肉屋</li>
+                    <li>和食</li>
+                </ul>
+            </li>
+            <li>好きなアイドル：KAT-TUN</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>おすすめのYouTubeチャンネル</h2>
+        <p><a href="https://www.youtube.com/@yoninochannel">yoninochannel</a></p>
+    </section>
+
+    <section>
+        <h2>今日5回聞いた歌</h2>
+        <p>timelesz ｢Anthem｣</p>
+        <p><a href="https://www.youtube.com/watch?v=omk3tMOIJZo&list=RDomk3tMOIJZo&start_radio=1">YouTubeで聴く</a></p>
+    </section>
+
+    <section>
+        <h2>HTMLサンプル</h2>
+        <pre>
+            &lt;html&gt;
+                &lt;head&gt;
+                    &lt;title&gt;Some HTML in here&lt;/title&gt;
+                &lt;/head&gt;
+                &lt;body&gt;
+                    &lt;h1&gt;Look ma! HTML!&lt;/h1&gt;
+                &lt;/body&gt;
+            &lt;/html&gt;
+        </pre>
+    </section>
+
+</body>
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
