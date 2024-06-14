98f19059a3d9cc38029422d71ff65ac19066cd4b
uuuuuuumi <166776882+uuuuuuumi@users.noreply.github.com>
2024-06-14 12:42:36 +0900

    HowOldAreYou

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse #インポート
+
 import random  # randomライブラリを追加
+
 
 app = FastAPI()
 
@@ -31,3 +34,17 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
